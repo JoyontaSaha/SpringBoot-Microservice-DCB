bc4a1cdf89899dc7ed8ec7e602b038f742eeb600
Joyonta Saha <joyontasaha8@gmail.com>
2021-12-19 20:09:46 +0600

    service-register api are added in cloud-api-gateway

---
+++ cloud-api-gateway/src/test/java/com/joyontasaha/cloudapigateway/CloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyontasaha.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
