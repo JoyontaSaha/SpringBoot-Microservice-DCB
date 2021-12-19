627bd98d9403fb4f7385a18525d0fb535f6416fa
Joyonta Saha <joyontasaha8@gmail.com>
2021-12-19 19:40:42 +0600

    department-service, user-service added to service-register

---
+++ service-registry/src/test/java/com/joyontasaha/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyontasaha.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
